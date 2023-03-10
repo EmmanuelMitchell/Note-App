e4720a728f7384219691b2fbf8ffbb0d7895d022
benjithorpe <benthorpe.dev@gmail.com>
2023-03-10 12:46:32 +0000

    switch to vite

---
+++ src/component/AddNote.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+	const [noteText, setNoteText] = useState("");
+	const reduseCharacter = 200;
+
+	const handleClick = (event) => {
+		if (reduseCharacter - event.target.value.length >= 0) {
+			setNoteText(event.target.value);
+		}
+	};
+
+	const handleSaveText = () => {
+		if (noteText.trim().length > 0) {
+			handleAddNote(noteText);
+			setNoteText("");
+		}
+	};
+
+	return (
+		<div className="note new">
+			<textarea
+				rows="8"
+				cols="10"
+				placeholder="Please add note here"
+				maxLength={200}
+				value={noteText}
+				onChange={handleClick}></textarea>
+
+			<div className="note-footer">
+				<small>{reduseCharacter - noteText.length} </small>
+				<button onClick={handleSaveText} className="save">
+					Save
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default AddNote;
